990b146ed8a6560f35beb5b183e675a8b7a5427a
Pythonic2 <igoormarinhosilva@gmail.com>
2024-07-06 17:48:24 -0300

    skjfl

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ options = ChromeOptions()
 options.set_capability('se:recordVideo', True)
 options.set_capability('se:screenResolution', '1920x1080')
 options.set_capability('se:name', 'test_visit_basic_auth_secured_page (ChromeTests)')
-driver = webdriver.Remote(options=options, command_executor="http://localhost:4444")
+driver = webdriver.Remote(options=options, command_executor="http://172.17.0.2:4444")
 driver.get("https://selenium.dev")
 print(driver.title)
-driver.quit()
+driver.quit()4
 
 # from selenium.webdriver import Keys
 # from selenium import webdriver
