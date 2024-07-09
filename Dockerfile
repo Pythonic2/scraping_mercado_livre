180b6c45f978df6d3ba94ffcad28385bb1b2997c
Pythonic2 <igoormarinhosilva@gmail.com>
2024-07-09 11:32:01 -0300

    tentando rodar se no portiner

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,2 @@
 # Use a imagem oficial do Python
-FROM python:3.11
-
-# Instale o Playwright via pip
-RUN pip install playwright
-
-# Configuração do Playwright para uso com Python
-RUN playwright install
-RUN playwright install-deps
-RUN apt-get install libgbm1
-
-# Diretório de trabalho para a aplicação
-WORKDIR /app
-
-# Copie os arquivos de aplicação para o contêiner
-COPY . .
-
-CMD ["python", "main.py"]
+FROM selenium/standalone-chrome:112.0
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import asyncio
-from playwright.async_api import async_playwright
+import os
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium import webdriver
 
-async def main():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto("http://playwright.dev")
-        print(await page.title())
-        await browser.close()
+# Obtém a URL do Selenium Remote a partir da variável de ambiente
+selenium_remote_url = os.getenv('SELENIUM_REMOTE_URL', 'http://172.17.0.3:4444')
 
-asyncio.run(main())
+options = ChromeOptions()
+options.set_capability('se:recordVideo', True)
+options.set_capability('se:screenResolution', '1920x1080')
+options.set_capability('se:name', 'test_visit_basic_auth_secured_page (ChromeTests)')
+
+driver = webdriver.Remote(options=options, command_executor=selenium_remote_url)
+driver.get("https://selenium.dev")
+print(driver.title)
+driver.quit()
