a2c16a528ae83934134e309319d6fd207e195120
Pythonic2 <igoormarinhosilva@gmail.com>
2024-07-09 13:32:45 -0300

    teste

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium import webdriver
 
 # Obtém a URL do Selenium Remote a partir da variável de ambiente
-selenium_remote_url = os.getenv('SELENIUM_REMOTE_URL', 'http://172.19.0.2:4444')
+selenium_remote_url = os.getenv('SELENIUM_REMOTE_URL', 'http://172.17.0.2:4444')
 
 options = ChromeOptions()
 options.set_capability('se:recordVideo', True)
@@ -13,4 +13,6 @@ options.set_capability('se:name', 'test_visit_basic_auth_secured_page (ChromeTes
 driver = webdriver.Remote(options=options, command_executor=selenium_remote_url)
 driver.get("https://selenium.dev")
 print(driver.title)
+from time import sleep
+sleep(120)
 driver.quit()
