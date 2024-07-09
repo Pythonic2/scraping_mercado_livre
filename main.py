ba3a8b723338bf289f7e88e6dbb46fbb84d2c2d7
Pythonic2 <igoormarinhosilva@gmail.com>
2024-07-09 17:44:37 -0300

    ok

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ options.set_capability('se:recordVideo', True)
 options.set_capability('se:screenResolution', '1920x1080')
 options.set_capability('se:name', 'test_visit_basic_auth_secured_page (ChromeTests)')
 
-options.set_capability('browserName','chrome')
-
 driver = webdriver.Remote(options=options, command_executor=selenium_remote_url)
 driver.get("https://selenium.dev")
 print(driver.title)
