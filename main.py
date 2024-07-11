69f537aa1ea7ac2570400877c07bb3dc6199e7d4
Pythonic2 <igoormarinhosilva@gmail.com>
2024-07-11 16:18:19 -0300

    ss

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import os
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium import webdriver
-
-# Obtém a URL do Selenium Remote a partir da variável de ambiente
-selenium_remote_url = os.getenv('SELENIUM_REMOTE_URL', 'http://0.0.0.0:4444')
-
-options = ChromeOptions()
-options.set_capability('se:recordVideo', True)
-options.set_capability('se:screenResolution', '1920x1080')
-options.set_capability('se:name', 'test_visit_basic_auth_secured_page (ChromeTests)')
-
-driver = webdriver.Remote(options=options, command_executor=selenium_remote_url)
-driver.get("https://selenium.dev")
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.firefox.options import Options
+# connect to grid
+driver = webdriver.Remote(
+    command_executor='http://172.18.0.2:4444',
+    options=Options(),
+)
+driver.get("https://google.com.br")
 print(driver.title)
-
-driver.quit()
