095860a6fe84feb6838ece09daa6f56401c2a016
Pythonic2 <igoormarinhosilva@gmail.com>
2024-07-16 21:19:47 -0300

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10
+FROM playw-bot-app:latest
 
 WORKDIR /app
 
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    page.goto("http://primeportalbr.com")
+    page.goto("http://youtube.com.br")
     print(page.title())
     browser.close()
